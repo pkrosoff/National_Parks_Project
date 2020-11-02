5f7e050321d759b5500f1635a85b6ad0eaf17379
fredmnsfld <fred.mnsfld@gmail.com>
2020-11-02 12:03:51 -0800

    updated fred.js and np_data_month

--- static/js/fred.js
+++ static/js/fred.js
@@ -3,95 +3,122 @@ function getPointCategoryName(point, dimension) {
         isY = dimension === 'y',
         axis = series[isY ? 'yAxis' : 'xAxis'];
     return axis.categories[point[isY ? 'y' : 'x']];
-}
-
-d3.json("http://127.0.0.1:5000/fred", function(data) {
-    console.log(data);
-});
-
-Highcharts.chart('container', {
-
-    chart: {
-        type: 'heatmap',
-        marginTop: 40,
-        marginBottom: 80,
-        plotBorderWidth: 1
-    },
+} 
 
 
-    title: {
-        text: 'Sales per employee per weekday'
-    },
-
-    xAxis: {
-        categories: ['January', 'february', 'March', 'April', 'May', 'June ', 'July', 'August', 'September', 'October', 'November', 'December']
-    },
-
-    yAxis: {
-        categories: ['xxxxx', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-        title: null,
-        reversed: true
-    },
-
-    accessibility: {
-        point: {
-            descriptionFormatter: function (point) {
-                var ix = point.index + 1,
-                    xName = getPointCategoryName(point, 'x'),
-                    yName = getPointCategoryName(point, 'y'),
-                    val = point.value;
-                return ix + '. ' + xName + ' sales ' + yName + ', ' + val + '.';
+d3.json("park_months").then(data=>
+    arrayLoop(data)
+);
+    function arrayLoop(data){
+        masterArray = [];
+        var months = data[0].months;
+        var parks = data[0].parks;
+        // console.log(parks);
+        months.forEach(month=>{
+            if(month=="January"){
+                monthWIP = 0;
             }
-        }
-    },
+            else{
+                monthWIP = monthWIP + 1;
+            }
+            // console.log(monthWIP) 
+            parks.forEach(park=>{
+                if(park=="Acadia"){
+                    parkWIP = 0;
+                }
+                else{
+                    parkWIP = parkWIP + 1;
+                }
+                addressWIP = [monthWIP,parkWIP,data[0][park][month]]
+                masterArray.push(addressWIP);
+            })
+        })
+        console.log(masterArray);
 
-    colorAxis: {
-        min: 0,
-        minColor: '#FFFFFF',
-        maxColor: Highcharts.getOptions().colors[0]
-    },
 
-    legend: {
-        align: 'right',
-        layout: 'vertical',
-        margin: 0,
-        verticalAlign: 'top',
-        y: 25,
-        symbolHeight: 280
-    },
+    Highcharts.chart('container', {
 
-    tooltip: {
-        formatter: function () {
-            return '<b>' + getPointCategoryName(this.point, 'x') + '</b> sold <br><b>' +
-                this.point.value + '</b> items on <br><b>' + getPointCategoryName(this.point, 'y') + '</b>';
-        }
-    },
+        chart: {
+            type: 'heatmap',
+            marginTop: 40,
+            marginBottom: 80,
+            plotBorderWidth: .5
+        },
 
-    series: [{
-        name: 'Sales per',
-        borderWidth: 1,
-        data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
-        dataLabels: {
-            enabled: true,
-            color: '#000000'
-        }
-    }],
+        title: {
+            text: 'Average Park Visits per Month'
+        },
 
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                yAxis: {
-                    labels: {
-                        formatter: function () {
-                            return this.value.charAt(0);
+        xAxis: {
+            categories: ['January', 'February', 'March', 'April', 'May', 'June ', 'July', 'August', 'September', 'October', 'November', 'December']
+        },
+
+        yAxis: {
+            categories: parks,
+            title: null,
+            reversed: true
+        },
+
+        accessibility: {
+            point: {
+                descriptionFormatter: function (point) {
+                    var ix = point.index + 1,
+                        xName = getPointCategoryName(point, 'x'),
+                        yName = getPointCategoryName(point, 'y'),
+                        val = point.value;
+                    return ix + '. ' + xName + ' sales ' + yName + ', ' + val + '.';
+                }
+            }
+        },
+
+        colorAxis: {
+            min: 0,
+            minColor: '#FFFFFF',
+            maxColor: Highcharts.getOptions().colors[0]
+        },
+
+        legend: {
+            align: 'right',
+            layout: 'vertical',
+            margin: 0,
+            verticalAlign: 'top',
+            y: 25,
+            symbolHeight: 280
+        },
+
+        tooltip: {
+            formatter: function () {
+                return '<b>' + getPointCategoryName(this.point, 'x') +
+                    this.point.value + '</b> visits to <br><b>' + getPointCategoryName(this.point, 'y') + '</b>';
+            }
+        },
+
+        series: [{
+            name: 'Visit per Month',
+            borderWidth: .2,
+            data: masterArray,
+            dataLabels: {
+                enabled: true,
+                color: '#000000'
+            }
+        }],
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return this.value.charAt(0);
+                            }
                         }
                     }
                 }
-            }
-        }]
-    }
+            }]
+        }
 
-});
+    });
+};
