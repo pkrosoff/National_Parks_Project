bb0d6cac004f24b5f01a989f1db352c15b7f201c
unknown <johnwhitakeraustin@gmail.com>
2020-11-14 08:52:36 -0800

    Do the thing!

--- app.py
+++ app.py
@@ -7,39 +7,39 @@ import requests
 
 app=Flask(__name__)
 CORS(app)
-app.config["DEBUG"]=True
-# app.config["MONGO_URI"] = os.environ['MONGO_URI']
-# mongo=PyMongo(app)
-# np_data = mongo.db.park_info
-# np_data_month = mongo.db.park_months
-# queryURL = "https://gist.githubusercontent.com/erincaughey/2f221501645757e28b715c4063e87595/raw/a90be1b434b1a8cdf71c2abc3373ca63987e2d23/nps-geo-boundary.json"
+# app.config["DEBUG"]=True
+app.config["MONGO_URI"] = os.environ['MONGO_URI']
+mongo=PyMongo(app)
+np_data = mongo.db.park_info
+np_data_month = mongo.db.park_months
+queryURL = "https://gist.githubusercontent.com/erincaughey/2f221501645757e28b715c4063e87595/raw/a90be1b434b1a8cdf71c2abc3373ca63987e2d23/nps-geo-boundary.json"
 
 @app.route("/")
 def index():
     return render_template(
         "index.html",
-        # MAPBOX=os.environ['MAPBOX']
+        MAPBOX=os.environ['MAPBOX'],
     )
 
-# @app.route("/parks_data")
-# def servemainData():
-#     return jsonify(list(np_data.find({ },
-#    { '_id': 0})))
+@app.route("/parks_data")
+def servemainData():
+    return jsonify(list(np_data.find({ },
+   { '_id': 0})))
 
-# @app.route("/park_months")
-# def servemonthData():
-#     return jsonify(list(np_data_month.find({ },
-#    { '_id': 0})))
+@app.route("/park_months")
+def servemonthData():
+    return jsonify(list(np_data_month.find({ },
+   { '_id': 0})))
 
-# @app.route("/park_boundaries")
-# def serveBoundaries():
-#     try:
-#         Uresponse = requests.get(queryURL)
-#     except requests.ConnectionError:
-#        return "Connection Error"  
-#     Jresponse = Uresponse.text
-#     data = json.loads(Jresponse)
-#     return Jresponse
+@app.route("/park_boundaries")
+def serveBoundaries():
+    try:
+        Uresponse = requests.get(queryURL)
+    except requests.ConnectionError:
+       return "Connection Error"  
+    Jresponse = Uresponse.text
+    data = json.loads(Jresponse)
+    return Jresponse
 
 if __name__=="__main__":
     app.run(debug=True)
