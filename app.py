adaeb16e70f9ae1e7a9f238ec5dd618e4c56bb24
unknown <johnwhitakeraustin@gmail.com>
2020-11-13 17:24:28 -0800

    Host. Please?

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import requests
 app=Flask(__name__)
 CORS(app)
 app.config["DEBUG"]=True
-app.config["MONGO_URI"] = os.environ['MONGO_URI']
-mongo=PyMongo(app)
+# app.config["MONGO_URI"] = os.environ['MONGO_URI']
+# mongo=PyMongo(app)
 # np_data = mongo.db.park_info
 # np_data_month = mongo.db.park_months
 # queryURL = "https://gist.githubusercontent.com/erincaughey/2f221501645757e28b715c4063e87595/raw/a90be1b434b1a8cdf71c2abc3373ca63987e2d23/nps-geo-boundary.json"
