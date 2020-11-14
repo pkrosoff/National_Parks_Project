a809b0a7acac7dacf31f55cb010d486b06096a80
unknown <johnwhitakeraustin@gmail.com>
2020-11-14 11:00:32 -0800

    Turned on debug

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 
 app=Flask(__name__)
 CORS(app)
-# app.config["DEBUG"]=True
+app.config["DEBUG"]=True
 app.config["MONGO_URI"] = os.environ['MONGO_URI']
 mongo=PyMongo(app)
 np_data = mongo.db.park_info
